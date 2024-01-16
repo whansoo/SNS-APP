fb07b66be39782af289d94c9300f39ee1aed576b
whansoo <ghkstn7523@naver.com>
2024-01-16 16:44:27 +0900

    feat: [front]  ant-design 라이브러리 문제 해결
    1.Next.js 14버전에서 ant-design import오류가 생김
    2.import * as React from 'react';,SyntaxError: Cannot use import statement outside a module
    3.ES6 모듈 문법인 import 문을 제대로 인식하지 못한것으로 보임
    4.nextjs.config.js에서 transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ], 를 추가해서 해결

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Menu } from 'antd'
 
 function AppLayout ({ children }: { children: React.ReactNode } ) {
+
+
+ 
   return (
     <div>
         <div>  
-            <Link href="/signup">노드버드</Link>
+        <Menu mode="inline">
+          <Menu.Item><Link href="/signup">노드버드</Link></Menu.Item>
+          <Menu.Item><Link href="/signup">프로필</Link></Menu.Item>
+          <Menu.Item><Link href="/signup">회원가입</Link></Menu.Item>
+        </Menu>
+        
+            
         </div>
         {children}
+
     </div>
   )
 }

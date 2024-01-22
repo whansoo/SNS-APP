e1c9b5421532dee77e2e53ff567dc51d34b3ed70
whansoo <ghkstn7523@naver.com>
2024-01-22 22:58:11 +0900

    feat: [back] express 설치 및 라우팅 테스트

--- back/app.js
+++ back/app.js
@@ -1,1 +1,28 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello express');
+});
+app.get('/api', (req, res) => {
+    res.send('hello api');
+});
+app.get('/api/posts', (req, res) => {
+    res.json([
+        { id:1, content: 'hello' },
+        { id:2, content: 'hello2' },
+        { id:3, content: 'hello3' },
+    ]);
+});
+app.post('/api/post', (req, res) => {
+    res.json('작성 완료');
+});
+app.delete('/api/delete', (req, res) => {
+    res.send('hello api');
+});
+
+
+app.listen(3065, () => {
+    console.log('서버 실행 중')
+});

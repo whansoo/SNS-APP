376e6b8096b39ecf576586440f86f6d09eed0e44
whansoo <ghkstn7523@naver.com>
2024-02-05 22:49:37 +0900

    feat: [back] middleware로 login 검사

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const { User, Post } = require('../models');
-
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
       console.log(err);
@@ -43,7 +43,7 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', isNotLoggedIn, async (req, res, next) => {
     try {
        const exUser = await User.findOne({
           where: {
@@ -67,7 +67,7 @@ router.post('/', async (req, res, next) => {
    
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
   res.send('ok');

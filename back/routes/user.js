7edc23d65d0dc6ae8450b79bca856ccac897c3c0
whansoo <ghkstn7523@naver.com>
2024-02-12 00:56:52 +0900

    feat: [back] 팔로우, 언팔로우 api 구현

--- back/routes/user.js
+++ back/routes/user.js
@@ -121,4 +121,32 @@ router.patch('/nickname', isLoggedIn, async (req, res, next) => {
   }
 })
 
+router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => { //PATCH  /user/1/follow
+  try {
+  const user = await User.findOne({ where: { id: req.params.userId }});
+  if (!user) {
+    res.status(403).send('존재하지 않는 사람을 팔로우하려고 하시네요');
+  }
+  await user.addFollowers(req.user.id);
+  res.status(200).json({ id: req.params.userId });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
+router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => { //DELETE  /user/1/follow
+  try {
+    const user = await User.findOne({ where: { id: req.params.userId }});
+    if (!user) {
+      res.status(403).send('존재하지 않는 사람을 언팔로우하려고 하시네요');
+    }
+    await user.removeFollowers(req.user.id);
+    res.status(200).json({ id: req.params.userId });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
 module.exports = router;
